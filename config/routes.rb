6ec179395cd0e1c048489e4abf36408c1539d9fd
victorcalisto <victorcalisto42@email.com>
2023-12-16 22:49:24 -0300

    finalizado rails 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/page1'
+  get 'pages/page2'
+  root 'pages#page1'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
